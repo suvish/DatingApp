c94a122e136e8c44d221fd29ee0214a4c0697d3e
Suvish Valsan <suvishvalsan@hotmail.com>
2018-09-20 07:17:31 +0530

    End of section 4

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -22,7 +22,7 @@ namespace DatingApp.Controllers
             _context = context;
         }
         // GET api/values
-       
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
